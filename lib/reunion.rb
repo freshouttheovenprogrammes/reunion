4112b63af721fb976db8c9bdc783835ad9711633
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-01-03 10:38:58 -0700

    still on it4

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,15 @@
+require_relative 'activity'
+
 class Reunion
+  attr_reader :location, :activities
+
+  def initialize(location, activity)
+    @location = location
+    @activities = [activity]
+  end
+
+  def add_activities(new_activity)
+    @activities << new_activity
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,23 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+
+  def test_attributes_of_reunion
+    reunion = Reunion.new('montana', 'hiking')
+
+    assert_equal 'montana', reunion.location
+    assert_equal ['hiking'], reunion.activities
+  end
+
+  def test_we_can_add_activity
+    reunion = Reunion.new('montana', 'hiking')
+    activity_1 = Activity.new('huckleberry_hunt')
+    reunion.add_activities(activity_1)
+
+    assert_equal ['hiking', activity_1], reunion.activities
+  end
+
 end
